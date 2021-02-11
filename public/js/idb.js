ef74571ebb1a150ac7dbd7f7b0a98ea0f3780735
Adam Barron <adambarron@me.com>
2021-02-10 22:03:29 -0800

    built out saveRecord function and uploadBudgetEntry function and verified with DevTools that indexedDB functionality works as expected

--- public/js/idb.js
+++ public/js/idb.js
@@ -18,3 +18,45 @@ request.onsuccess = function(event) {
 request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget_entry'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget_entry');
+
+    budgetObjectStore.add(record);
+};
+
+function uploadBudgetEntry() {
+    const transaction = db.transaction(['new_budget_entry'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget_entry');
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_budget_entry'], 'readwrite');
+                    const budgetObjectStore = transaction.objectStore('new_budget_entry');
+                    budgetObjectStore.clear();
+
+                    alert('All saved budget entries have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        };
+    }
+}
+
+window.addEventListener('online', uploadBudgetEntry);
